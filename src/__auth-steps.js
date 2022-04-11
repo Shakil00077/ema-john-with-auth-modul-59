b5166b7bfb9e941e5e8edfabc53ab95afde7c00b
unknown <shakilshehjaad@gmail.com>
2022-04-11 13:08:17 +0600

    initialize google log in authentication

---
+++ src/__auth-steps.js
@@ -0,0 +1,7 @@
+/**
+ * 1. create a new project console.google.com
+ * 2. npm install firebase
+ * 3. create firebase.init.js and import gerAuth to export auth
+ * 4. firebase settings > Authintecation > enable email and password auth
+ * 
+ * **/ 
