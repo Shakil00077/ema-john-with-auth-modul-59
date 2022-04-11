f8d685e67a4d44bceca0202d8932d99259ebdf83
unknown <shakilshehjaad@gmail.com>
2022-04-11 14:16:25 +0600

    create login page

--- src/__auth-steps.js
+++ src/__auth-steps.js
@@ -3,5 +3,6 @@
  * 2. npm install firebase
  * 3. create firebase.init.js and import gerAuth to export auth
  * 4. firebase settings > Authintecation > enable email and password auth
+ * 5. 
  * 
  * **/ 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <div>
+                <h2>Login</h2>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="Email">Email</label>
+                        <input type="email" placeholder='Email' name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Password">Password</label>
+                        <input type="password" placeholder='Password' name="password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="LogIn" />
+                </form>
+                <p className='form-account'>
+                    New to Ema-John? <Link className='form-link' to='/signup'>Create an account</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
